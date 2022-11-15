2296f00a855295db67e6a699811dba9f1e77075d
William Gadd <wgadd@sasktel.net>
2022-11-15 02:43:54 +0000

    Add labels to join employee and store tables

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,8 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Richard", last_name: "Lokey", hourly_rate: 80)
+@store1.employees.create(first_name: "Terry", last_name: "Robertson", hourly_rate: 89)
+@store2.employees.create(first_name: "Harry", last_name: "Bonds", hourly_rate: 120)
+@store2.employees.create(first_name: "George", last_name: "Ranger", hourly_rate: 16)
